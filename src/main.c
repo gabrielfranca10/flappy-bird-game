c1e7609243b35cf2cc61fbef58e4b8952a34859c
Luis guilherme alves lauria soares <luisguilherme.lauria@gmail.com>
2025-05-15 13:11:26 -0300

    Inicializando o jogo com a raylib

--- src/main.c
+++ src/main.c
@@ -11,44 +11,23 @@ by Jeffery Myers is marked with CC0 1.0. To view a copy of this license, visit h
 
 #include "resource_dir.h"	// utility header for SearchAndSetResourceDir
 
-int main ()
-{
-	// Tell the window to use vsync and work on high DPI displays
-	SetConfigFlags(FLAG_VSYNC_HINT | FLAG_WINDOW_HIGHDPI);
-
-	// Create the window and OpenGL context
-	InitWindow(1280, 800, "Hello Raylib");
-
-	// Utility function from resource_dir.h to find the resources folder and set it as the current working directory so we can load from it
-	SearchAndSetResourceDir("resources");
-
-	// Load a texture from the resources directory
-	Texture wabbit = LoadTexture("wabbit_alpha.png");
+int main (void) {
 	
-	// game loop
-	while (!WindowShouldClose())		// run the loop untill the user presses ESCAPE or presses the Close button on the window
-	{
-		// drawing
+	// Inicializando o  tamanho da tela do jogo
+	const int screenWidth = 800; 
+	const int screenHeight = 600;
+
+	InitWindow(screenWidth, screenHeight, "Flappy Bird - Raylib");
+
+	while (!WindowShouldClose()) {
 		BeginDrawing();
+		ClearBackground(RAYWHITE);
 
-		// Setup the back buffer for drawing (clear color and depth buffers)
-		ClearBackground(BLACK);
+		DrawText("Seja bem-vindo, Flappy Bird", 190, 200, 20, LIGHTGRAY);
 
-		// draw some text using the default font
-		DrawText("Hello Raylib", 200,200,20,WHITE);
-
-		// draw our texture to the screen
-		DrawTexture(wabbit, 400, 200, WHITE);
-		
-		// end the frame and get ready for the next one  (display frame, poll input, etc...)
 		EndDrawing();
 	}
 
-	// cleanup
-	// unload our texture so it can be cleaned up
-	UnloadTexture(wabbit);
-
-	// destroy the window and cleanup the OpenGL context
 	CloseWindow();
 	return 0;
 }
